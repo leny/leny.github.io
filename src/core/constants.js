f984236727e362db1848c30852c6feffe23ba463
leny <info@flatland.be>
2019-03-04 22:06:14 +0100

    Import core components from kourikoulom

---
+++ src/core/constants.js
@@ -0,0 +1,23 @@
+/* leny/leny.github.io
+ *
+ * /src/core/constants.js - Constants & stuffs
+ *
+ * coded by leny@flatLand!
+ * started at 04/03/2019
+ */
+
+export const NBSP = "\u00a0";
+
+export const BCG_COLOR = "#fff";
+export const MAIN_COLOR = "#000";
+export const FADE_COLOR = "#666";
+export const ALT_COLOR = "royalblue";
+export const ALT_HOVER_COLOR = "mediumblue";
+export const BORDER_COLOR = "#eee";
+
+export const SOURCE_SANS_PRO_STACK = `"Source Sans Pro", "Helvetica Neue", Helvetica, Arial, sans-serif`;
+
+export const MQ_TABLET = "min-width: 640px";
+export const MQ_SMALL_DESKTOP = "min-width: 960px";
+export const MQ_MEDIUM_DESKTOP = "min-width: 1280px";
+export const MQ_LARGE_DESKTOP = "min-width: 1520px";
---
+++ src/core/font-awesome.js
@@ -0,0 +1,14 @@
+/* leny/leny.github.io
+ *
+ * /src/core/font-awesome.js - FontAwesome Library
+ *
+ * coded by leny@flatLand!
+ * started at 04/03/2019
+ */
+
+import {library} from "@fortawesome/fontawesome-svg-core";
+
+import {fas} from "@fortawesome/pro-solid-svg-icons";
+import {fab} from "@fortawesome/free-brands-svg-icons";
+
+library.add(fab, fas);
