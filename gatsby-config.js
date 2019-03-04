81c2953b1dc627e14ea13291d4f9079e91026efe
leny <info@flatland.be>
2019-03-04 22:03:30 +0100

    Import gatsby config

---
+++ gatsby-config.js
@@ -0,0 +1,34 @@
+/* leny/leny.github.io
+ *
+ * /gatsby-config.js - Gatsby main configuration file
+ *
+ * coded by leny@flatLand!
+ * started at 28/02/2019
+ */
+
+module.exports = {
+    plugins: [
+        "gatsby-transformer-json",
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `data`,
+                path: `${__dirname}/data/`,
+            },
+        },
+        {
+            resolve: `gatsby-plugin-emotion`,
+        },
+        {
+            resolve: `gatsby-plugin-prefetch-google-fonts`,
+            options: {
+                fonts: [
+                    {
+                        family: `Source Sans Pro`,
+                        variants: [`400`, `600`],
+                    },
+                ],
+            },
+        },
+    ],
+};
